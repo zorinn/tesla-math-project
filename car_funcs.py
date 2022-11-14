ce813021f849a884a14a0e16de8dcc70f2cc9d13
Zorin Makabenta <30711340+zorinn@users.noreply.github.com>
2022-11-14 16:39:13 -0500

    car_funcs.py - Added helper functions

--- car_funcs.py
+++ car_funcs.py
@@ -1,3 +1,5 @@
+import math
+
 class Car:
 
     def __init__(self, mass, drag_coefficient, tire_radius, static_friction, surface_area,
@@ -17,3 +19,37 @@ class Car:
         self.r_2 = r_2
         self.max_velocity = max_velocity
         self.r_max = r_max
+
+def find_R(v,car):
+    return (60*car.gear_reduction*v)/(2*math.pi*car.tire_radius)
+
+def f_m_of_R(R,car):
+
+    if R <= car.r_1:
+        result = (car.gear_reduction*car.t_max) / car.tire_radius
+    
+    elif R > car.r_1 and R < car.r_2:
+        numerator1 = ((9549.3*car.gear_reduction*car.p_max) / car.r_2) - (car.gear_reduction*car.t_max)
+        result = (numerator1 / (car.r_2 - car.r_1))*((R-car.r_1)/car.tire_radius)+((car.gear_reduction*car.t_max)/car.tire_radius)
+
+    elif R >= car.r_2:
+        result = (9549.3*car.gear_reduction*car.p_max) / (car.tire_radius * R)
+    
+    return result
+
+def H_of_v(v,car):
+
+    minimum1 = car.static_friction*car.mass*car.gravitational_acceleration
+    R = find_R(v,car)
+    minimum2 = f_m_of_R(R,car)
+
+    if minimum1 < minimum2:
+        minimum = minimum1
+    else:
+        minimum = minimum2
+    
+    in_parentheses = minimum - (0.5)*car.air_density*car.drag_coefficient*car.surface_area*(v**2)
+    result = (1/car.mass)*(in_parentheses)
+
+    return result
+
