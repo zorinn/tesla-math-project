f53fba076d300eba59c523bc905582d8677f6a9f
zorinn <zorinmakabenta@live.com>
2022-12-01 19:40:29 +0000

    Added full write_to_csv functionality

--- main.py
+++ main.py
@@ -8,26 +8,32 @@ def euler_method(v,car):
     v_n_plus_1 = v + car_funcs.H_of_v(v,car)*DELTA_T
     return v_n_plus_1
 
-def write_to_csv(csv_file,dict):
+def write_to_csv(csv_file,data):
+    csv_headers = ['miniseconds','velocity']
 
-    with open(csv_file, 'w') as f:
-        for key in dict.keys():
-            f.write("%s,%s"%(key,dict[key]))
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile,fieldnames=csv_headers)
+        writer.writeheader()
+        for dict in data:
+            writer.writerow(dict)
 
 
 def main():
 
     tesla_model_s_p100d = car_funcs.Car(2250,0.24,(14.15*0.0254),1.11,2.1,980,450.4,4000,5750,150,16614)
 
-    v_dict = {}
+    v_data = []
     v = 0
     
     for i in range(2000):
+        v_dict = dict()
         v1 = euler_method(v,tesla_model_s_p100d)
-        v_dict[i] = v1 * 2.23694 # meters per second to mph
+        v_dict['miniseconds'] = i
+        v_dict['velocity'] = v1 * 2.23694 # meters per second to mph
+        v_data.append(v_dict)
         v = v1
 
-    write_to_csv('tesla_model_s_no_torque_factor.csv',v_dict)
+    write_to_csv('tesla_model_s_no_torque_factor.csv',v_data)
 
 if __name__ == "__main__":
     main()
